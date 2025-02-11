3b419cfd7f7a6b2a48a9cbc9e14105d9270d7649
Herman <hermansyah_@live.com>
2025-02-11 10:21:51 +0700

    Update RekeningPenyesuaianSeeder.php

--- database/seeders/RekeningPenyesuaianSeeder.php
+++ database/seeders/RekeningPenyesuaianSeeder.php
@@ -17,6 +17,12 @@ class RekeningPenyesuaianSeeder extends Seeder
         $json = file_get_contents(database_path('seeders/rekening_penyesuaian.json'));
         $data = json_decode($json, true);
 
+        // Konversi format datetime
+        foreach ($data as &$item) {
+            $item['created_at'] = Carbon::parse($item['created_at'])->format('Y-m-d H:i:s');
+            $item['updated_at'] = Carbon::parse($item['updated_at'])->format('Y-m-d H:i:s');
+        }
+
         // Masukkan data ke dalam database
         DB::table('rekening_penyesuaian')->insert($data);
     }
