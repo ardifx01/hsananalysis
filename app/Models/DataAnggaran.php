c378f3ed75a38daa35a653dd8f332c2ec7d5d1bb
Herman <hermansyah_@live.com>
2025-05-21 18:54:16 +0700

    perbaikan upload data_anggaran

--- app/Models/DataAnggaran.php
+++ app/Models/DataAnggaran.php
@@ -11,17 +11,30 @@ class DataAnggaran extends Model
     use HasFactory;
 
     protected $fillable = [
+        'tahun',
+        'kode_urusan',
+        'nama_urusan',
         'kode_skpd',
         'nama_skpd',
         'kode_sub_unit',
         'nama_sub_unit',
+        'kode_bidang_urusan',
+        'nama_bidang_urusan',
+        'kode_program',
+        'nama_program',
+        'kode_kegiatan',
+        'nama_kegiatan',
         'kode_sub_kegiatan',
         'nama_sub_kegiatan',
+        'kode_sumber_dana',
+        'nama_sumber_dana',
         'kode_rekening',
         'nama_rekening',
+        'kode_standar_harga',
+        'nama_standar_harga',
         'pagu',
-        'tahapan_id', // Tambahkan tahapan_id
-        'tanggal_upload', // Tambahkan tanggal_upload
+        'tahapan_id',
+        'tanggal_upload',
     ];
 
     protected $dates = ['tanggal_upload']; // Pastikan tanggal_upload di-cast sebagai date
